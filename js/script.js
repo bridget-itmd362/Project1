5852dbe359bf56280190bf5f87d714af886fd4c3
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2023-10-31 22:35:44 -0600

    Got JavaScript to work
    At first removed async from the index file,  which allowed my js to work, however, it was stated in the instructions to have it included. That is why i added window.onload to ensure that js code runs after the entire page is loaded.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+//async was not working until i included window.onload
+//ensures that js code runs after the entire page is loaded
+window.onload = function(){
   const myButton = document.querySelector('input[type="submit"]');
 
   myButton.addEventListener("click", function() {
-    console.log("Hello World!");
+    alert("thank you for your submission")
   });
-});
+}
