3f482b7ce9ed89e566a6bcafe9e975de9574b5d5
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2023-10-31 21:19:43 -0600

    Added javascript  event listener
    Currently having trouble with my code, so I included the console.log to see if it might be an HTML issue and not js issue.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const myButton = document.querySelector('.button');
+
+  myButton.addEventListener('click', function () {
+      console.log("Hello world!");
+  });
+});
