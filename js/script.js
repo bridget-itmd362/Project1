0cd5a40d46027fe7cc40655fb6abb97df4c8aa79
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2023-11-01 23:58:29 -0600

    Added Javascript validation to text fields.
    Added javascript validation to text fields, which was supposed to include both first name and last name, however, it fails when one of them is true and the other is false.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 //async was not working until i included window.onload
 //ensures that js code runs after the entire page is loaded
 window.onload = function(){
-  const myButton = document.querySelector('input[type="submit"]');
+  const button = document.querySelector('input[type="submit"]');
 
-  myButton.addEventListener("click", function() {
-    alert("thank you for your submission")
-  });
+  button.addEventListener("click", myFunction);
+
+  function myFunction(){
+    //check if text inputs get numbers /^[A-Za-z]+$
+    const letters = /[a-zA-Z]/;
+    let textInp = document.querySelector('input[type="text"]');
+    if(letters.test(textInp.value) == false){
+      alert("Please input a valid name");
+    } else {
+      alert("Thank you for signing up to our mailing list!");
+    }
+  }
 }
