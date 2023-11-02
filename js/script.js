cc988436fe844533572cb94476a67b17e333bf30
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2023-11-02 00:07:13 -0600

    Fixed Javascript validation to text fields
    Was able to check each text input field, to see if the inputs for the first and last names were numbers.

--- js/script.js
+++ js/script.js
@@ -8,9 +8,12 @@ window.onload = function(){
   function myFunction(){
     //check if text inputs get numbers /^[A-Za-z]+$
     const letters = /[a-zA-Z]/;
-    let textInp = document.querySelector('input[type="text"]');
-    if(letters.test(textInp.value) == false){
-      alert("Please input a valid name");
+    let firstInp = document.querySelector('input[id="fname"]');
+    let lastInp = document.querySelector('input[id="lname"]');
+    if(letters.test(firstInp.value) == false){
+      alert("Please input a valid first name");
+    } else if (letters.test(lastInp.value) == false){
+      alert("Please input a valid last name");
     } else {
       alert("Thank you for signing up to our mailing list!");
     }
